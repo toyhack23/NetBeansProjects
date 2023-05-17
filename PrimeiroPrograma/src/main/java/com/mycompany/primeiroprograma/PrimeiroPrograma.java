cde7bbc82c47649df1e2e83c15a8214671b45531
Emerson Vinicius Monteiro <112833766+toyhack23@users.noreply.github.com>
2023-05-16 22:10:28 -0300

    Initial commit

---
+++ PrimeiroPrograma/src/main/java/com/mycompany/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.primeiroprograma;
+
+/**
+ *
+ * @author Emerson
+ */
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!!");
+        System.out.println("Emerson");
+    }
+}
